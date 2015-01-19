102cf1ab3d28116b4ced434a738fda1b6c509fbd
migueldiaz <migueldiaz@outlook.com>
2015-01-19 19:35:10 +0100

    Añadiendo reales

--- app/controllers/desglose_controller.rb
+++ app/controllers/desglose_controller.rb
@@ -5,6 +5,7 @@ class DesgloseController < ApplicationController
 
   	@muebles=@pedido.muebles
 
+  	@mobiliario=@pedido.items.first.componentes_mueble
   	@cascos= Componente.joins(:mueble).where("tipo = 'CASCO'")#.where(:tipo=>'CASCO')
   	@puertas=@pedido.componentes.where("tipo = 'PUERTA'")
   	@herrajes=@pedido.componentes.where("tipo = 'HERRAJE'")
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ActiveRecord::Base
   belongs_to :pedido
   belongs_to :modelo
   #autocomplete :modelo, :nombre
+  def componentes_mueble
+  	return self.modelo.mueble.componentes
+  end
+  def componentes_modelo
+  	return self.modelo.componentes
+  end
+  ## en componente necesito multiplicar el modelo * la pieza 
+  ## en el total necesito multiplicar la cantidad de items por el modelo
+  ## Un join ?????
 end

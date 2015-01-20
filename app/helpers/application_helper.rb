9a35e5d75e9911e6a7ab0ff7fb1bbe8404970814
migueldiaz <migueldiaz@outlook.com>
2015-01-20 23:05:18 +0100

    FUNCIONA!! los totales

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def totalpuertas(items)
+    	coleccion=Componente.none
+    	items.each do|total|
+    		coleccion =coleccion + (total.dame_puertas)
+  		end
+  		return coleccion
+  	end
 end
--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -6,12 +6,72 @@ class Pedido < ActiveRecord::Base
 
   has_many :muebles, :through => :modelos
   has_many :componentes, :through => :modelos
-
- # def componentes_mueble
  
-  #	self.items.each do |item|
-  #		componentes_mueble << item.componentes_mueble
-  #	end
-  #	retun componentes_mueble
-  #end
+  #-------------------------------------
+  def dame_puertas_pedido
+    componentes_puerta=Componente.none
+       self.items.each do |item|
+    componentes_puerta = componentes_puerta + item.modelo.componentes.where(:tipo=>'Puerta')
+    end
+    return componentes_puerta
+  end
+  def dame_puertas_unicas
+    return self.dame_puertas_pedido.uniq{|x| x.pieza_id}
+  end
+  def total_puertas(pieza)
+      suma=0
+      self.dame_puertas_pedido.each do |componente|
+        if componente.pieza==pieza
+          suma=suma+componente.cantidad
+        end
+      end
+      return suma;
+  end
+
+  #-------------------------------------
+  def dame_herrajes_pedido
+    componentes_herraje=Componente.none
+       self.items.each do |item|
+    componentes_herraje = componentes_herraje + item.modelo.componentes.where(:tipo=>'Herraje')
+    end
+    return componentes_herraje
+  end
+  def dame_herrajes_unicas
+    return self.dame_herrajes_pedido.uniq{|x| x.pieza_id}
+  end
+  def total_herrajes(pieza)
+      suma=0
+      self.dame_herrajes_pedido.each do |componente|
+        if componente.pieza==pieza
+          suma=suma+componente.cantidad
+        end
+      end
+      return suma;
+  end
+
+
+  #-----------------------------------
+  def componentes_mueble
+    componentes_mueble=Componente.none
+    self.items.each do |item|
+    componentes_mueble = componentes_mueble + item.modelo.mueble.componentes
+    end
+    return componentes_mueble
+  end
+  #-------------------------------------
+  def componentes_mueble_unicos
+    
+  	return self.componentes_mueble.uniq{|x| x.pieza_id}
+  end
+  #------------------------------------------
+  def total_pieza(pieza)
+      suma=0
+      self.componentes_mueble.each do |componente|
+        if componente.pieza==pieza
+          suma=suma+componente.cantidad
+        end
+      end
+      return suma;
+  end
+  #----------------------------------------------
 end
